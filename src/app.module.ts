b9cbe3e4f36d050accdb337cff70448617c52e6d
Jeon-man <jhmjhm0106@naver.com>
2025-03-12 15:40:45 +0900

    CHORE: Remove unused module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,11 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import DatabaseModule from './db/database.module';
 import SurveyModule from './survey/survey.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
-    DatabaseModule,
     SurveyModule,
   ],
   controllers: [AppController],
