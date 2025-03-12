6eb87e5475487eb61d35c2497a228fe247e57833
Jeon-man <jhmjhm0106@naver.com>
2025-03-12 15:37:55 +0900

    FIX: Import prisma

--- src/survey/survey.module.ts
+++ src/survey/survey.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SurveyController } from './survey.controller';
 import { SurveyService } from './survey.service';
+import PrismaModule from '../db/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule],
   providers: [SurveyService],
   controllers: [SurveyController],
   exports: [],
