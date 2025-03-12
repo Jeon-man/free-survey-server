121212cef03a46e4fd8297d25182d503f51f5f2e
Jeon-man <jhmjhm0106@naver.com>
2025-03-12 16:36:05 +0900

    FIX: Wrong fastify serverless

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,27 @@
-import { HttpAdapterHost, NestFactory } from '@nestjs/core';
+import { NestFactory } from '@nestjs/core';
 import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { VercelRequest, VercelResponse } from '@vercel/node';
-import { Application } from 'express';
+
 import { AppModule } from './app.module';
+
+type HTTPMethods =
+  | 'DELETE'
+  | 'delete'
+  | 'GET'
+  | 'get'
+  | 'HEAD'
+  | 'head'
+  | 'PATCH'
+  | 'patch'
+  | 'POST'
+  | 'post'
+  | 'PUT'
+  | 'put'
+  | 'OPTIONS'
+  | 'options';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -20,8 +36,14 @@ async function bootstrap() {
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   const app = await bootstrap();
 
-  const adapterHost = app.get(HttpAdapterHost);
-  const httpAdaptor = adapterHost.httpAdapter;
-  const instance = httpAdaptor.getInstance<Application>();
-  instance(req, res);
+  const fastify = app.getHttpAdapter().getInstance();
+
+  const result = await fastify.inject({
+    method: (req.method as HTTPMethods) || 'GET',
+    url: req.url || '/',
+    headers: req.headers,
+    payload: req.body as unknown as Record<string, any>,
+  });
+
+  res.status(result.statusCode).send(result.body);
 }
