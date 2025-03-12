a295972b1c70bd7e28a79de3625260516e0527ee
Jeon-man <jhmjhm0106@naver.com>
2025-03-12 09:16:28 +0900

    FIX: Inject config

--- src/db/database.module.ts
+++ src/db/database.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { SequelizeProvider } from './sequelize.provider';
 
 @Module({
   imports: [
     SequelizeModule.forRootAsync({
+      inject: [ConfigModule],
       useExisting: SequelizeProvider,
     }),
   ],
-  exports: [SequelizeModule],
 })
 export default class DatabaseModule {}
