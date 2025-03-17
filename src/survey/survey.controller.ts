41a50e77b22e7cbe5865a42127cb63872e5cef77
Jeon-man <jhmjhm0106@naver.com>
2025-03-17 14:16:15 +0900

    REFACTOR: Remove transaction

--- src/survey/survey.controller.ts
+++ src/survey/survey.controller.ts
@@ -1,13 +1,11 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CreateSurveyBody } from './survey.dto';
 import { SurveyService } from './survey.service';
-import { Transactional } from '../db/transaction.decorator';
 
 @Controller('surveys')
 export class SurveyController {
   constructor(private readonly surveyService: SurveyService) {}
 
-  @Transactional()
   @Post()
   async createSurvey(@Body() createSurveyData: CreateSurveyBody) {
     return this.surveyService.createSurvey(createSurveyData);
